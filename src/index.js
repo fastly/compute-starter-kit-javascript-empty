052a80482c7c76ccbed252cced12567c4b102d29
Journie Ma-Johnson <journie.johnson@fastly.com>
2023-01-26 16:20:08 -0800

    adding service version logging

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@
 addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
 
 async function handleRequest(event) {
+  // Log service version
+  console.log("FASTLY_SERVICE_VERSION:", fastly.env.get('FASTLY_SERVICE_VERSION' || ''));
+  
   return new Response("OK", { status: 200 });
 }
